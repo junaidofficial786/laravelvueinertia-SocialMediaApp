7c7ec221dbf0f72a7819f187ac452d06bc38fb40
Junaid Hassan <junaidhassan225588@gmail.com>
2024-05-19 13:43:02 +0500

    homepage frontend completed

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,23 +4,27 @@ import GroupList from "@/Components/app/GroupList.vue";
 import FollowerList from "@/Components/app/FollowerList.vue";
 import PostList from "@/Components/app/PostList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 </script>
 
 <template>
   <Head title="Social Media Website" />
-
-  <!-- layout using tailwind starts-->
-  <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-    <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-      <group-list />
+  <AuthenticatedLayout>
+    <!-- layout using tailwind starts-->
+    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+      <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+        <group-list />
+      </div>
+      <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+        <follower-list />
+      </div>
+      <div
+        class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
+      >
+        <create-post />
+        <post-list class="flex-1" />
+      </div>
     </div>
-    <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-      <follower-list />
-    </div>
-    <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-      <create-post />
-      <post-list class="flex-1"/>
-    </div>
-  </div>
+  </AuthenticatedLayout>
   <!-- layout using tailwind ends-->
 </template>
