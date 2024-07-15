e413ac6f6c7b3bd6a0761098fd82932e9529a001
Junaid Hassan <junaidhassan225588@gmail.com>
2024-07-15 13:43:03 +0500

    completed cover and avatar uploading

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -58,15 +58,6 @@
           </div>
           <div class="flex flex-1 p-4 items-center justify-between">
             <h2 class="font-bold text-lg">{{ user.name }}</h2>
-            <PrimaryButton v-if="isMyProfile">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="size-4 mr-2">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-              </svg>
-
-              Edit Profile
-            </PrimaryButton>
           </div>
         </div>
 
